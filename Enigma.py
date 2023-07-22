3ae7f499fc5df32d8ccebfd9d9c174b6b13474e1
ars1992 <alessandro.salomon@gmx.de>
2023-07-22 12:58:29 +0200

    verschlüsselung test

--- Enigma.py
+++ Enigma.py
@@ -30,3 +30,15 @@ class Enigma:
         self.r3_pos = r3_pos
         self.c_offset = int('A'.encode("ascii")[0])
 
+
+    def encrypt_char(self, c: str) -> str:
+        step0 = self.patch_key[int(c.encode("ascii")[0]) - self.c_offset]
+        step1 = self.r1[(int(step0.encode("ascii")[0]) - self.c_offset + (self.position // len(self.al) ** 2) + self.r1_pos % len(self.al))]
+        step2 = self.r2[(int(step1.encode("ascii")[0]) - self.c_offset + (self.position // len(self.al)) + self.r2_pos) % len(self.al)]
+        step3 = self.r3[(int(step2.encode("ascii")[0]) - self.c_offset + self.position + self.r3_pos) % len(self.al)]
+        step4 = self.re[int(step3.encode("ascii")[0]) - self.c_offset]
+        step5 = self.r3[(int(step4.encode("ascii")[0]) - self.c_offset + self.position + self.r3_pos) % len(self.al)]
+        step6 = self.r2[(int(step5.encode("ascii")[0]) - self.c_offset + (self.position // len(self.al)) + self.r2_pos) % len(self.al)]
+        step7 = self.r1[(int(step6.encode("ascii")[0]) - self.c_offset + (self.position // len(self.al) ** 2) + self.r1_pos) % len(self.al)]
+        step8 = self.patch_key[int(step7.encode("ascii")[0]) - self.c_offset]
+        self.position += 1
